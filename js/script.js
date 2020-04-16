321a7b8780feb3c50b7f516ed90512ac72001a41
Timothy Frame <t.f.retouching@gmail.com>
2020-04-16 14:38:39 +0100

    align comments

--- js/script.js
+++ js/script.js
@@ -37,21 +37,21 @@ const showPage = (list, page) => {
         it removes the active class from all a elements using a for loop and then assigns
         the a element that was clicked an active className;  */
 const appendPageLinks = (list) => {
-//.page div called, newDiv and ul element created
+        //.page div called, newDiv and ul element created
   const pageDiv = document.querySelector('.page');
   const newDiv = document.createElement('div');
   newDiv.className = 'pagination';
   const ul = document.createElement('ul');
 
-      //numberOfPages variable declared. Use to calculate how many pages need to be generated.
+        //numberOfPages variable declared. Use to calculate how many pages need to be generated.
   let listLength = list.length;
   let numberOfPages = Math.ceil( listLength / itemsPerPage )
 
-      //newDiv and  ul appended to .page div
+        //newDiv and  ul appended to .page div
   pageDiv.appendChild(newDiv);
   newDiv.appendChild(ul);
 
-      //for loop used to create and apend new li and a elements to NewDiv
+        //for loop used to create and apend new li and a elements to NewDiv
   for( let i = 0; i < numberOfPages; i += 1){
 
   const listItem = document.createElement('li');
@@ -66,25 +66,25 @@ const appendPageLinks = (list) => {
 
 
   }
-      //first li element in newDiv is called, stored in active link variable. Class name on first li ic hanged to active.
+        //first li element in newDiv is called, stored in active link variable. Class name on first li ic hanged to active.
   const activeLink = document.querySelector('li a:first-child');
   activeLink.className = 'active';
 
-      //showPage function called
+        //showPage function called
   showPage(itemList, 1)
-      // new varibale declared calling all 'a' elements
+        // new varibale declared calling all 'a' elements
   const aLinks = document.querySelectorAll('a');
-      /* click event is added to all 'a' elements. 'active' name class is removed from
-      all 'a' elemebnts. 'active class is added to 'a element that has been clicked */
+        /* click event is added to all 'a' elements. 'active' name class is removed from
+        all 'a' elemebnts. 'active class is added to 'a element that has been clicked */
   ul.addEventListener('click', (event) => {
     for( let c = 0; c < aLinks.length; c += 1){
       aLinks[c].className = '';
     }
     event.target.className = 'active';
-      //showPage function called displaying active links
+        //showPage function called displaying active links
     showPage(itemList, event.target.textContent)
   });
 
 }
-    //appendPageLinks function called
+        //appendPageLinks function called
 appendPageLinks(itemList)
